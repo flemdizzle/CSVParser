d5efe52da4cc7ac1eeac677ceae875bf10320326
flemdizzle <fleminsh@gmail.com>
2015-01-13 17:21:01 -0500

    it's creating a json file in output

--- parser.js
+++ parser.js
@@ -1,19 +1,29 @@
 var fs = require("fs");
 var fileName = "NTD.csv";
-var outputFileName = "/output/NTD.json";
+var outputFileName = "output/NTD.json";
 
 
 fs.readFile(fileName, "utf8", function(err, data){
-  
+  var output = {};
+  output["name"]= "hello";
+  data.substr(0,13);
+  writeJSON(JSON.stringify(output));
+
 });
 
-fs.writeFile(outputFileName, "derp", function(err){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("JSON saved to " + outputFileName);
-  }
-});
+function writeJSON(data){
+  fs.writeFile(outputFileName, data, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("JSON saved to " + outputFileName);
+      fs.readFile(outputFileName, "utf8", function(err, data){
+        console.log(data);
+      });
+    }
+  });
+}
+
 
 
 
